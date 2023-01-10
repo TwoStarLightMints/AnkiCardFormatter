a8d7f8a6648368f72836ccdbf997ebf06936bb85
KC Bomonti <mondo.beyondo3@gmail.com>
2023-01-10 18:00:07 -0500

    Fix whitespace

--- card_gen.py
+++ card_gen.py
@@ -35,7 +35,7 @@ class CardGenerator:
         contents: list[str] = combined.split("|")
         front: str = contents[0]
         back: str = contents[1]
-    
+
     def parse_content(self, content: str):
         results: list[str] = list()
 
@@ -53,13 +53,13 @@ class CardGenerator:
                 results.append(n_g)
             if g != ("", ""):
                 results.append(g)
-        
+
         return results
-    
+
     def find_chunk(self, content: str, start_ind=0) -> list[str, tuple[str], int]:
         # Find the content that is marked for gramaticalization
         ind1 = content.find("-", start_ind)
-        
+
         if ind1 == -1:
             return [content[start_ind:len(content)], ("", ""), -1]
 
@@ -67,7 +67,7 @@ class CardGenerator:
         # Find the grammaticalization rules
         g_ind1 = content.find("<", ind1)
         g_ind2 = content.find(">", g_ind1 + 1)
-        
+
         return [content[start_ind:ind1], (content[ind1+1:ind2], content[g_ind1+1:g_ind2]), g_ind2+1] # The plus one in the first part of the slice is to remove the delimiting marker from the resulting chunk
 
 if __name__ == "__main__":
