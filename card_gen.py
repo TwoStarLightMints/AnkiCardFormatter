d6e741d6ebca69c5695b7a7b236018723eafb45a
KC Bomonti <mondo.beyondo3@gmail.com>
2023-01-10 14:46:12 -0500

    Declare CardGenerator

---
+++ card_gen.py
@@ -0,0 +1,10 @@
+
+# Class should be given a front and back plain text as to be able to create proper cards
+
+# Front:
+#   {Front content}
+# Back:
+#   {Back content}
+
+class CardGenerator:
+    pass
