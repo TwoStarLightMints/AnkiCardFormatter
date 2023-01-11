053bf3801a7091ccc7aee4bfb34d9dbd84ae1c24
KC Bomonti <mondo.beyondo3@gmail.com>
2023-01-10 23:05:22 -0500

    Testing capabilities

--- write_anki_txt.py
+++ write_anki_txt.py
@@ -1,6 +1,7 @@
 class AnkiTxtWriter:
     def __init__(self, origin_file: str, separator:str="\t", html:str="false", deck_column:str="1", tags_column:str="4", deck:str="German Vocab::General"):
         self.card_contents = self.get_card_contents(origin_file)
+        self.create_anki_txt_file(separator, html, deck_column, tags_column, deck)
 
     def get_card_contents(self, path):
         contents = list()
@@ -16,4 +17,10 @@ class AnkiTxtWriter:
             anki.write(f"html:{html}")
             anki.write(f"#deck column:{deck_col}")
             anki.write(f"tags column:{tag_col}")
+
             # From here on, write the different cards
+            for card in self. card_contents:
+                anki.write(f"{deck}\t{card}")
+
+if __name__ == "__main__":
+    AnkiTxtWriter("anki.txt")
