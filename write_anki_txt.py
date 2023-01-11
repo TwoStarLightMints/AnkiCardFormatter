e9b77f0f84bf1f262150a5dbc2e3b8f080e12c8d
KC Bomonti <mondo.beyondo3@gmail.com>
2023-01-10 23:06:46 -0500

    Add new line characters to end of writes in create_anki_txt_file

--- write_anki_txt.py
+++ write_anki_txt.py
@@ -13,14 +13,14 @@ class AnkiTxtWriter:
     
     def create_anki_txt_file(self, sep: str, html: str, deck_col: str, tag_col: str, deck: str):
         with open("to_import.txt", "w") as anki:
-            anki.write(f"#separator:{sep}")
-            anki.write(f"html:{html}")
-            anki.write(f"#deck column:{deck_col}")
-            anki.write(f"tags column:{tag_col}")
+            anki.write(f"#separator:{sep}\n")
+            anki.write(f"html:{html}\n")
+            anki.write(f"#deck column:{deck_col}\n")
+            anki.write(f"tags column:{tag_col}\n")
 
             # From here on, write the different cards
             for card in self. card_contents:
-                anki.write(f"{deck}\t{card}")
+                anki.write(f"{deck}\t{card}\n")
 
 if __name__ == "__main__":
     AnkiTxtWriter("anki.txt")
