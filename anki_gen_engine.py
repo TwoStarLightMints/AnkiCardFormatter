4f05ab24cf9ff56c3218a98eaf2ba78e96a54cf8
KC Bomonti <mondo.beyondo3@gmail.com>
2023-01-11 23:31:32 -0500

    Add deck info for testing

--- anki_gen_engine.py
+++ anki_gen_engine.py
@@ -1,12 +1,13 @@
+from sys import argv
+
 from card_gen import CardGenerator
 from write_anki_txt import AnkiTxtWriter
 
 class Engine:
+    IMPORT_FROM_EXT = ".txt"
     def __init__(self, path, separator:str="\t", html:str="false", deck_column:str="1", tags_column:str="4", deck:str="German Vocab::General"):
-        self.generator = CardGenerator(path)
+        self.generator = CardGenerator(f"{path}{self.IMPORT_FROM_EXT}")
         self.writer = AnkiTxtWriter(self.generator.processed, separator, html, deck_column, tags_column, deck)
 
-        self.writer.create_anki_txt_file(separator, html, deck_column, tags_column, deck)
-
 if __name__ == "__main__":
-    eng = Engine("anki.txt")
+    eng = Engine(argv[1], deck="TESTING_PY")
